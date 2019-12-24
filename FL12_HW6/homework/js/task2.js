896a91c4cc8255b30072e8a99947338d1fda43ff
VStoilovskyi <vadimstoylovskiy@gmail.com>
2019-12-25 00:00:25 +0200

    updated hv#6

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -3,16 +3,16 @@ const b = + prompt('Enter value#2').trim();
 const c = + prompt('Enter value#3').trim();
 
 if ( !parseInt(a) || !parseInt(b) || !parseInt(c) ) {
-   alert('input values should be ONLY numbers');
+   console.log('input values should only be numbers');
 } else if ( a <= 0 || b <= 0 || c <= 0  ) {
-    alert('A triangle must have 3 sides with a positive definite length');
+    console.log('Numbers must be positive');
 } else {
     if ( a + b <= c || a + c <= b || b + c <= a ) {
         console.log('Triangle cannot exist');
-    } else if (a === c && a === b) {
-        console.log('Equilateral triangle');
     } else if (a !== b && b !== c && a !== c) {
         console.log('Scalene triangle');
+    } else if (a === c && a === b) {
+        console.log('Equilateral triangle');
     } else {
         console.log('Isosceles triangle');
     }
